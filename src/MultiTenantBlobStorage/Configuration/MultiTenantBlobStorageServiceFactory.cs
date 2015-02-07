f99be3b924644705b8265c47af90ddce623540ef
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-02-07 22:20:11 +0100

    missing files

---
+++ src/MultiTenantBlobStorage/Configuration/MultiTenantBlobStorageServiceFactory.cs
@@ -0,0 +1,20 @@
+﻿using SInnovations.Azure.MultiTenantBlobStorage.Logging;
+using SInnovations.Azure.MultiTenantBlobStorage.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityModel.Owin.ResourceAuthorization;
+
+namespace SInnovations.Azure.MultiTenantBlobStorage.Configuration
+{
+    public class MultiTenantBlobStorageServiceFactory
+    {
+        static readonly ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        public Registration<ITenantContainerNameService> TenantContainerNameService { get; set; }
+        public Registration<IStorageAccountResolverService> StorageAccountResolver { get; set; }
+        public Registration<IResourceAuthorizationManager> AuthorizationManager { get; set; }
+    }
+}
---
+++ src/MultiTenantBlobStorage/Services/Default/UnityDependencyResolver.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SInnovations.Azure.MultiTenantBlobStorage.Services.Default
+{
+    internal class UnityDependencyResolver : IDependencyResolver
+    {
+        readonly IUnityContainer ctx;
+        public UnityDependencyResolver(IUnityContainer ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public T Resolve<T>(string name)
+        {
+            if (name != null)
+            {
+                return ctx.Resolve<T>(name);
+            }
+
+            return ctx.Resolve<T>();
+        }
+    }
+}
