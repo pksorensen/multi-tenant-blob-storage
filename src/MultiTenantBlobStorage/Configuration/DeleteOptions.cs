c853bdc39cee5d71912f1680c6fbc3cc7f64a8ad
Poul Kjeldager Sørensen <pks@s-innovations.net>
2015-02-10 00:32:53 +0100

    missing file

---
+++ src/MultiTenantBlobStorage/Configuration/DeleteOptions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SInnovations.Azure.MultiTenantBlobStorage.Configuration
+{
+    public class ReplacementOperation
+    {
+        public string Method{get;set;}
+        public string Query{get;set;}
+        public Func<Stream> Content{get;set;}
+        public Action<IOwinResponse> Response { get; set; }
+    }
+    public class DeleteOptions
+    {
+        public Func<IOwinRequest, ReplacementOperation> OnDelete { get; set; }
+
+        public Func<IDictionary<string, string>> SetMetaDataOnDelete { get; set; }
+    }
+}
